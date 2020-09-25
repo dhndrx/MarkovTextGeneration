80dae041b1d4d144ae9ed0361cccfd724b681608
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-09-24 21:26:09 -0500

    Update WordGram.java

--- WordGram.java
+++ WordGram.java
@@ -16,7 +16,6 @@ public class WordGram {
     }
 
     public int length(){
-        // TODO: Complete this method
         return myWords.length;
     }
 
@@ -31,7 +30,6 @@ public class WordGram {
 
     public boolean equals(Object o) {
         WordGram other = (WordGram) o;
-        // TODO: Complete this method
         if(other.length()!=myWords.length){
             return false;
         }
@@ -47,8 +45,7 @@ public class WordGram {
     public WordGram shiftAdd(String word) { 
         WordGram out = new WordGram(myWords, 0, myWords.length);
         // shift all words one towards 0 and add word at the end. 
-        // you lose the first word
-        // TODO: Complete this method
+        // first word is lost
         for(int i=0;i<myWords.length-1;i++){
             myWords[i]=myWords[i+1];
         }
