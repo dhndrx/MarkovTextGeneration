4ec9323d5b99432e9e13331afff36904290240b3
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-09-24 21:23:56 -0500

    Update IMarkovModel.java

--- IMarkovModel.java
+++ IMarkovModel.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of interface IMarkovModel here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 public interface IMarkovModel {
     public void setTraining(String text);
     
