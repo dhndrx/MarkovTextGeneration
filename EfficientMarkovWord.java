19472adb8d626e2f9654f5375e2dadb0eb937dd0
Deontee Hendricks <48738169+dhndrx@users.noreply.github.com>
2020-09-24 21:23:19 -0500

    Update EfficientMarkovWord.java

--- EfficientMarkovWord.java
+++ EfficientMarkovWord.java
@@ -1,11 +1,3 @@
-
-/**
- * Write a description of EfficientMarkovWord here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-
 import java.util.*;
 
 public class EfficientMarkovWord implements IMarkovModel{
